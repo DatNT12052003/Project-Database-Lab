54a47a3adec488cde1528e46234ac5c1b5cc7919
DatNT12052003 <thanhdat2003pt@gmail.com>
2025-03-14 17:38:44 +0700

    first commit

---
+++ DatabaseLab/src/model/person/Student.java
@@ -0,0 +1,17 @@
+package model.person;
+
+public class Student extends Person{
+	private String studentid;
+	
+	public Student(String studentid) {
+		super();
+		this.studentid = studentid;
+	}
+
+	public String getStudentid() {
+		return studentid;
+	}
+	public void setStudentid(String studentid) {
+		this.studentid = studentid;
+	}
+}
